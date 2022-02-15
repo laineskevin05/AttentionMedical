815f0887843f05726ee236c365ef3558bb8036af
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-02-14 22:21:59 -0600

    Update LoginPage.js

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -1,29 +1,30 @@
-
-import Fondo from '../assets/img.svg';
-import IconUser from '../assets/user.svg';
+import Fondo from "../assets/img.svg";
+import IconUser from "../assets/user.svg";
 
 const LoginPage = () => {
   return (
     <>
-    <div className="grid grid-cols-5 bg-indigo-600 ">
-      <div className="col-span-3 bg-slate-800	">
-        <div
-          className="  w-full h-full"
-  
-        >
-          <img src={Fondo}  alt="Logo"/>
-        </div>
-      </div>
-      <div className="col-span-2 bg-gray-100 content-center break-words place-items-center pt-16">
-        <div className="mb-4">
-          <div className="grid w-full place-items-center">
-            <img src={IconUser} alt="Icon User" className=" rounded-full border-2 mt-4 mb-1  border-gray-600" width="80px"/>
+      <div className="grid grid-cols-5 bg-indigo-600 ">
+        <div className="col-span-3 bg-slate-800	">
+          <div className="  w-full h-full">
+            <img src={Fondo} alt="Logo" />
           </div>
-          <h6 className="mt-6 mb-3 uppercase text-gray-600 text-lg text-center font-bold">
-            Inicio de Sesion
-          </h6>
-          <form>
-            <div className=" w-full mb-3 px-14">
+        </div>
+        <div className="col-span-2 bg-gray-100 content-center break-words place-items-center pt-16">
+          <div className="mb-4">
+            <div className="grid w-full place-items-center">
+              <img
+                src={IconUser}
+                alt="Icon User"
+                className=" rounded-full border-2 mt-4 mb-1  border-gray-600"
+                width="80px"
+              />
+            </div>
+            <h6 className="mt-6 mb-3 uppercase text-gray-600 text-lg text-center font-bold">
+              Inicio de Sesion
+            </h6>
+            <form>
+              <div className=" w-full mb-3 px-14">
                 <label
                   className="block uppercase text-gray-700 text-xs font-bold"
                   htmlFor="grid-password"
@@ -41,7 +42,7 @@ const LoginPage = () => {
                 <label className="block uppercase text-gray-700 text-xs font-bold">
                   Contraseña
                 </label>
-                <input 
+                <input
                   type="password"
                   className="mt-1 border-0 py-3 rounded shadow text-sm w-full placeholder-gray-400 text-gray-600 bg-white"
                   placeholder="Contraseña"
@@ -49,54 +50,46 @@ const LoginPage = () => {
                 />
               </div>
               <div className="w-full mb-6 px-14">
-                  <label className="inline-flex items-center cursor-pointer">
-                    <input
-                      id="customCheckLogin"
-                      type="checkbox"
-                      className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
-                      style={{ transition: "all .15s ease" }}
-                    />
-                    <span className="ml-2 text-sm font-semibold text-gray-700">
-                      Recuerdame
-                    </span>
-                  </label>
-                </div>
-                <div className="text-center mt-6 px-14">
-                  <button
-                    className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                    type="button"
+                <label className="inline-flex items-center cursor-pointer">
+                  <input
+                    id="customCheckLogin"
+                    type="checkbox"
+                    className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
                     style={{ transition: "all .15s ease" }}
-                  >
-                    Iniciar Sesion
-                  </button>
-                </div>
+                  />
+                  <span className="ml-2 text-sm font-semibold text-gray-700">
+                    Recuerdame
+                  </span>
+                </label>
+              </div>
+              <div className="text-center mt-6 px-14">
+                <button
+                  className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  Iniciar Sesion
+                </button>
+              </div>
             </form>
             <div className="grid grid-cols-2 px-14 mt-4">
               <div className="text-sm font-bold text-blue-400">
-                <a
-                  href="#aa"
-                  onClick={e => e.preventDefault()}
-                >
+                <a href="#aa" onClick={(e) => e.preventDefault()}>
                   Olvidaste tu Contraseña?
                 </a>
               </div>
               <div className="font-bold text-gray-700">
-                <a
-                  href="#pp"
-                  onClick={e => e.preventDefault()}
-                >
+                <a href="#pp" onClick={(e) => e.preventDefault()}>
                   Registrarse
                 </a>
               </div>
             </div>
+          </div>
         </div>
+        <div className="col-span-full top-0 w-full h-24 bg-gray-900"></div>
       </div>
-      <div
-        className="col-span-full top-0 w-full h-24 bg-gray-900"
-      ></div>
-    </div>
-      
     </>
-  )}
+  );
+};
 
 export default LoginPage;
