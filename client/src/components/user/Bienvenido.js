f9b7c1ff691caaa575aa0bd181d0468cfa0e1ac0
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-02-21 14:09:22 -0600

    Update Bienvenido.js

--- client/src/components/user/Bienvenido.js
+++ client/src/components/user/Bienvenido.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Bienvenido = () => {
-  const { uid } = useSelector((state) => state.auth);
-
+  const { name } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   return (
     <>
       <div>
-        <Link to="/login">Bienvenido </Link>
+        <span> Bienvenido {name} </span>
+        <button
+          className="bg-red-600 p-5"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/cambiarcontrasenia");
+          }}
+        >
+          cambiar contrasenia
+        </button>
       </div>
     </>
   );
