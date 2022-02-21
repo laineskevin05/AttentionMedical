a6e944d640fa03550d66e808b6b155f283bc9c82
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-02-21 13:34:37 -0600

    Update Bienvenido.js

--- client/src/components/user/Bienvenido.js
+++ client/src/components/user/Bienvenido.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Navbar from "../ui/Navbar";
 
 const Bienvenido = () => {
-  //onst dispatch = useDispatch();
   const { uid } = useSelector((state) => state.auth);
 
-  //const navigate = useNavigate();
   return (
     <>
       <div>
