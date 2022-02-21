28057a5f10f6c4899e9c25a235b7786f7636c46f
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-02-21 00:45:30 -0600

    Cambio de version

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/tailwind.css";
-import { MainRouter } from "./routers/MainRouter";
-//import RegisterPage from "./views/RegisterPage";
-import LoginPage from "./views/LoginPage";
-import RegisterPage from "./views/RegisterPage";
-
-ReactDOM.render(<MainRouter/>, document.getElementById("root"));
