9ddd5da98726a0211d7f1f41c0b02ac9c7585ad0
BrayanBustillo17 <75953520+BrayanBustillo17@users.noreply.github.com>
2022-02-21 20:00:52 -0600

    CambioContraseña

---
+++ client/src/components/auth/CambioContraseña.js
@@ -0,0 +1,60 @@
+    import React from 'react'
+    import "./CambioContraseña.css"
+    
+    const CambioContraseña = () => {
+      return (
+
+
+
+
+        
+
+            <section id='global'>
+
+                <header>
+
+                    <div id="logo"> <h1>CAMBIAR CONTRASEÑA</h1>  </div>
+                   
+                </header>
+
+
+
+
+
+                <div id=" logo ">
+                   
+
+                <form> 
+
+                
+                <input name='contraseñaN' type="tex" placeholder=" CONTRASEÑA NUEVA"      />
+
+                <input name='contraseñaC' type="tex" placeholder=" CONFIRMAR CONTRASEÑA"      />
+
+                <input type="submit" value="CAMBIAR CONTRASEÑA"/>
+                </form>
+
+
+                </div>
+
+
+
+
+
+            </section>
+        
+
+
+
+
+       
+    
+
+
+
+
+
+      )
+    }
+    
+    export default CambioContraseña
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/tailwind.css";
+  import "./assets/tailwind.css";
 
 import { App } from "./App";
 
+//import CambioContraseña from "./components/auth/CambioContraseña";
 ReactDOM.render(<App />, document.getElementById("root"));
+ 
