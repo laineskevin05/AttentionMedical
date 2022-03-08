38715d42da19ad32a8d3e282171ebeeee5bc498e
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-03-08 12:04:44 -0600

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        principal: "#fe3",
+        secundario: "#ba0",
+      },
+    },
   },
   plugins: [],
-}
+};
