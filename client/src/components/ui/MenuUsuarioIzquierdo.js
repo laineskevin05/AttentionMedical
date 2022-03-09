19cc97eaf309b53fc01a03693812242d22c47a23
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-03-08 21:31:08 -0600

    color

--- client/src/components/ui/MenuUsuarioIzquierdo.js
+++ client/src/components/ui/MenuUsuarioIzquierdo.js
@@ -5,23 +5,23 @@ const MenuUsuarioIzquierdo = () => {
   return (
     <div className="bg-menu_izquierdo  w-1/5 inline-block ">
       <div>Menu Usuario Izquierdo</div>
-      <div className="flex  w-full justify-end my-2 ">
+      <div className="flex  w-full justify-end my-2 px-2 ">
         <Link to="/nuevacita" className="w-1/2">
-          <button className="bg-red-600 rounded text-white p-4 items-center w-full ">
+          <button className="bg-gray-100 rounded px-2 p-4 items-center w-full ">
             Nueva Cita
           </button>
         </Link>
       </div>
-      <div className="flex  w-full justify-end my-2 ">
+      <div className="flex  w-full justify-end px-2 my-2 ">
         <Link to="/" className="w-1/2">
-          <button className="bg-red-600 rounded text-white p-4 items-center w-full">
+          <button className="bg-gray-100 rounded  p-4 items-center w-full">
             Inicio
           </button>
         </Link>
       </div>
-      <div className="flex  w-full justify-end my-2 ">
+      <div className="flex  w-full justify-end my-2  px-2">
         <Link to="/perfil" className="w-1/2">
-          <button className="bg-red-600 rounded text-white p-4 items-center w-full">
+          <button className="bg-gray-100 rounded  p-4 items-center w-full">
             Perfil
           </button>
         </Link>
