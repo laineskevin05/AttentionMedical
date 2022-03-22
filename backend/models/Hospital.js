91cdd836a8c462771b3328d1ee865020cae84ea0
jose-117 <54189782+jose-117@users.noreply.github.com>
2022-03-22 10:54:53 -0600

    Backend de Hospital

---
+++ backend/models/Hospital.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const HospitalSchema = Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contrasenia: {
+    type: String,
+    required: true,
+  },
+  telefono: {
+    type: String,
+    required: true,
+  },
+  direccion: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  }
+});
+
+HospitalSchema.method('toJSON', function(){
+    const {__v, _id, ...object} = this.toObject();
+    object.id = _id;
+    return object;
+})
+module.exports = model("Hospital", HospitalSchema);
---
+++ backend/routes/hospital.js
@@ -0,0 +1,58 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+
+const {
+
+    crearCentroMedico,
+    loginCentroMedico,
+    revalidarToken,
+    cargarCentroMedico
+} = require ("../controllers/centroMedico");
+
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const router = Router();
+
+router.post(
+    "/new",
+    [
+      // middlewares
+      check("nombre", "El nombre es obligatorio").not().isEmpty(),
+      check("correo", "El correo es obligatorio").isEmail(),
+      check(
+        "contrasenia",
+        "La Contrasenia debe de ser de 6 o mas caracteres"
+      ).isLength({ min: 6 }),
+      check("telefono", "El telefono es obligatorio").not().isEmpty(),
+      check("direccion", "La direccion es obligatoria").not().isEmpty(),
+      check("descripcion", "la descipcion es obligatoria").not().isEmpty(),
+      
+      
+  
+      validarCampos,
+    ],
+    crearCentroMedico
+  );
+
+
+
+  router.post(
+    "/",
+    [
+      check("correo", "El correo es obligatorio").isEmail(),
+      check("contrasenia", "La contrasenia debe de ser de 6 caracteres").isLength(
+        {
+          min: 6,
+        }
+      ),
+      validarCampos,
+    ],
+    loginCentroMedico
+  );
+
+  router.get("/renew", validarJWT, revalidarToken);
+
+  router.get("/perfil/:id", validarJWT, cargarCentroMedico );
+
+  module.exports = router;
